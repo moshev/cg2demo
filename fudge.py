2a6faf423826bc64994b54c1f84c2bfa4e12fb63
Velichko Karakostov <vivo@specto.org>
2014-09-28 18:06:36 +0300

    Cubes

--- fudge.py
+++ fudge.py
@@ -19,11 +19,11 @@ VS_SRC = br'''
 #version 150
 
 in vec2 p;
-out vec4 ray;
+out vec3 ray;
 
 void main() {
     vec4 pos = vec4(p, 0.5, 1.0);
-    ray = vec4(vec2(0.5, 0.5) - p, -1.0, 1.0);
+    ray = vec3(p, -1.0);
     ray = normalize(ray);
     gl_Position = pos;
 }
@@ -33,11 +33,47 @@ void main() {
 FS_SRC = br'''
 #version 150
 
-in vec4 ray;
+in vec3 ray;
 out vec4 color;
 
+float dist_object(vec3 p) {
+    vec3 bmin = vec3(-1.3, -1.3, -3.3);
+    vec3 bmax = vec3(-0.3, -0.3, -1.0);
+    vec3 dmin = bmin - p;
+    vec3 dmax = p - bmax;
+    vec3 max1 = max(dmin, dmax);
+    vec2 max2 = max(max1.xy, max1.z);
+    return max(max2.x, max2.y);
+}
+
+float shade(vec3 p) {
+    vec3 t1 = cross(ray, vec3(1, 0, 0));
+    if (dot(t1, t1) < 0.001) {
+        t1 = cross(ray, vec3(0, 1, 0));
+    }
+    t1 = normalize(t1);
+    vec3 t2 = normalize(cross(ray, t1));
+    vec3 p1 = p + 0.0001 * t1;
+    vec3 p2 = p + 0.0001 * t2;
+    p1 = p1 + ray * dist_object(p1);
+    p2 = p2 + ray * dist_object(p2);
+    return dot(normalize(cross(p2 - p, p1 - p)), vec3(0, -1, 0));
+}
+
 void main() {
-    color = vec4(0.5, 0.5, 0.5, 1.0);
+    vec3 p = vec3(0.0, 0.0, 1.0);
+    float d = dist_object(p);
+    float epsilon = 1.0e-6;
+    for (int i = 0; i < 1000; i++) {
+        if (d > epsilon) {
+            p += d * ray;
+            d = dist_object(p);
+        }
+    }
+    if (abs(d) > epsilon) {
+        discard;
+    }
+    color = vec4(vec3(1.0, 1.0, 1.0) * shade(p), 1.0);
 }
 '''
 
@@ -65,10 +101,10 @@ def render_thread(window):
     GL.glBindVertexArray(vao)
     QUAD = util.GLBuffer(12)
     with QUAD.bound:
-        QUAD[:] = numpy.array([0, 0,
-                               1, 0,
+        QUAD[:] = numpy.array([-1, -1,
+                               1, -1,
                                1, 1,
-                               0, 1],
+                               -1, 1],
                               dtype=numpy.float32)
         GL.glEnableVertexAttribArray(attrloc)
         GL.glVertexAttribPointer(attrloc, 2, GL.GL_FLOAT, GL.GL_FALSE, 0, None)
