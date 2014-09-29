cc6bbc942e6184a3310b33725158bb4deed8532c
Moshev <mo6eeeb@gmail.com>
2014-09-29 23:31:43 +0300

    rotations and proper morphings and oh my

--- fudge.py
+++ fudge.py
@@ -5,27 +5,24 @@
 
 import sdl2
 import sys
-import threading
-import time
-import queue
 import util
 import numpy
 from sdl2 import *
 if (SDL_InitSubSystem(SDL_INIT_VIDEO) != 0):
     print("WAAH!")
-    sys.exit(1);
+    sys.exit(1)
 
 VS_SRC = br'''
 #version 150
 
 in vec2 p;
-out vec3 ray;
+out vec3 inray;
 
 void main() {
     vec2 p1 = p * vec2(1.0, 1024.0 / 768.0);
     vec4 pos = vec4(p1, 0.5, 1.0);
-    ray = vec3(p * 0.8, -1.0);
-    ray = normalize(ray);
+    inray = vec3(p, -1.0);
+    inray = normalize(inray);
     gl_Position = pos;
 }
 '''
@@ -35,13 +32,15 @@ FS_SRC = br'''
 #version 150
 
 uniform int millis;
-in vec3 ray;
+in vec3 inray;
 out vec4 color;
+
+vec3 ray;
 
 /*cube*/
 float cube(vec3 p) {
-    vec3 bmin = vec3(-1.3, -1.3, -3.3);
-    vec3 bmax = vec3(-0.3, -0.3, -1.0);
+    vec3 bmin = vec3(-0.7, -0.7, -0.7);
+    vec3 bmax = vec3(-0.2, -0.2, -0.2);
     vec3 dmin = bmin - p;
     vec3 dmax = p - bmax;
     vec3 max1 = max(dmin, dmax);
@@ -55,19 +54,34 @@ float sphere(vec3 p, vec3 c, float r) {
     return distance(c, p) - r;
 }
 
+float timing(int period) {
+    return float(millis % period) / float(period - 1);
+}
+
 float dist_object(vec3 p) {
-    float t = 2.0 * abs(fract(millis / 5000.0) - 0.5);
-//    t = 0.7;
-//    return cube(p);
-    return (1.0 - t) * cube(p) + t * sphere(p, vec3(-1.0, -1.0, -2.0), 0.5);
-//    return cube(p) / (sphere(p, vec3(0.0, 0.0, -2.0), 0.2) + 1.0);
+    float t = timing(4000);
+    t = 2.0 * abs(t - 0.5);
+    float u = (1.0 - t);
+    // this piece is nonsensical but without it
+    // we get a black screen, fuck you nVidia
+    // fuck you with a rusty rake
+    if (u < 0.0) {
+        u = 0.0;
+    }
+    vec3 centre = vec3(-0.2, -0.2, -0.4);
+    return (u * sphere(p, centre, 0.25) + t * cube(p));
+/*
+    return cube(p + vec3(sin(3.141259 * fract(p.x + t)),
+                         sin(3.141259 * fract(p.y + t)),
+                         sin(3.141259 * fract(p.z + t))));
+                        */
 }
 
 
 vec4 trace(vec3 p) {
     float d = dist_object(p);
     float epsilon = 4.0e-07;
-    for (int i = 0; i < 1000; i++) {
+    for (int i = 0; i < 1024; i++) {
         if (d > epsilon) {
             p += d * ray;
             d = dist_object(p);
@@ -100,12 +114,25 @@ vec3 shade(vec3 p, vec3 c) {
         p2 = q2.xyz;
     }
     vec3 n = normalize(cross(p1 - p, p2 - p));
-    return min((max(dot(n, light1), 0.0) + max(dot(n, light2), 0.0)) * 0.5 * c, c);
+    return min((max(dot(n, light1), 0.0) +
+           max(dot(n, light2), 0.0)) * 0.5 * c, c);
 //    return (vec3(1.0, 1.0, 1.0) + normalize(cross(p1 - p, p2 - p))) * 0.5;
 }
 
 void main() {
     vec3 p = vec3(0.0, 0.0, 1.0);
+    float angle = 6.28318530717958647692 * timing(20000);
+    vec3 axis = normalize(vec3(-0.2, 1.0, 0.3));
+    mat3x3 rotmat = mat3(vec3(1.0, 0.0, 0.0),
+                         vec3(0.0, 1.0, 0.0),
+                         vec3(0.0, 0.0, 1.0));
+    rotmat = rotmat * cos(angle);
+    rotmat = rotmat + sin(angle) * mat3(vec3(0.0, axis.z, -axis.y),
+                                        vec3(-axis.z, 0.0, axis.x),
+                                        vec3(axis.y, -axis.x, 0.0));
+    rotmat = rotmat + (1.0 - cos(angle)) * outerProduct(axis, axis);
+    p = rotmat * p;
+    ray = rotmat * inray;
     vec4 q = trace(p);
     if (q.w < 1.0) {
         discard;
@@ -161,7 +188,8 @@ def render_thread(window):
         t_start = time.monotonic()
         GL.glClear(GL.GL_COLOR_BUFFER_BIT)
         GL.glUseProgram(PROG.id)
-        PROG['millis'] = SDL_GetTicks();
+        #PROG['millis'] = 1  # SDL_GetTicks()
+        PROG['millis'] = SDL_GetTicks()
         GL.glDrawArrays(GL.GL_TRIANGLE_FAN, 0, 4)
         GL.glUseProgram(0)
         SDL_GL_SwapWindow(window)
@@ -180,7 +208,11 @@ SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE)
 SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3)
 SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3)
 
-window = SDL_CreateWindow(b"Super Special Awesome Demo 2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1024, 768,
-                          SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_RESIZABLE)
+window = SDL_CreateWindow(b"Super Special Awesome Demo 2",
+                          SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                          1024, 768,
+                          SDL_WINDOW_OPENGL |
+                          SDL_WINDOW_ALLOW_HIGHDPI |
+                          SDL_WINDOW_RESIZABLE)
 
 render_thread(window)
