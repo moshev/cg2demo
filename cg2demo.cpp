ff99d552b4ed213adbf633d6db8d65a2c682c848
Димитър Тошев <Mo6eeeB@gmail.com>
2014-10-15 08:08:28 +0000

    Got transitions to work sort of nicely

--- cg2demo.cpp
+++ cg2demo.cpp
@@ -436,7 +436,7 @@ static int renderloop(SDL_Window *window, SDL_GLContext context) {
         }
         glClear(GL_COLOR_BUFFER_BIT);
         if (progs[scene].ufrm_millis > 0) {
-            glUniform1i(progs[scene].ufrm_millis, ticks_start - scene_start);
+            glUniform1i(progs[scene].ufrm_millis, ticks_start - ticks_first);
         }
         camera = mkcamera(ticks_start - ticks_first, mktranslationm4(scenes[scene].camera_translation));
         glUniformMatrix4fv(progs[scene].ufrm_camera, 1, 0, &camera.c[0].v[0]);
