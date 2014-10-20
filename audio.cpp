be8d20e11c680429e9b72c88129ddf83183d338b
Moshev <Mo6eeeB@gmail.com>
2014-10-20 20:15:16 +0000

    Revert "Third layer"

--- audio.cpp
+++ audio.cpp
@@ -9,10 +9,6 @@ std::atomic_int current_scene;
 // i.e. 0 1 1 1 0 1 1 (plus 1 on each)
 //static const uint8_t scale = 0x3B;
 
-int8_t notesoffsets_DOREMI[] = {
-    0, 2, 4, 5, 7, 9, 11, 12, 14, 16, 17, 19, 21, 23, 24, 26
-};
-
 int8_t notesoffsets_BLUES[] = {
     3, 6, 8, 9, 10, 13, 15, 18, 20, 21, 22, 25, 27, 30, 32, 33, 35,
 };
@@ -26,8 +22,8 @@ int8_t notesoffsets_DEEP_BLUES[] = {
 };
 
 //HUNGARIAN MINOR
-int8_t notesoffsets_HUNGARIAN[] = {
-    3, 5, 6, 9, 10, 11, 15, 16, 18, 19, 22, 23, 24, 25, 26, 27
+uint16_t noteshz_HUNGARIAN[] = {
+    262, 294, 311, 370, 392, 415, 494, 523, 587, 622, 734, 784, 831, 988, 1047, 1175
 };
 
 #define NOTESOFFSETS notesoffsets_BLUES
@@ -150,15 +146,21 @@ double audio_gen_1(audio_state *as) {
     double attack = 0.02;
     double sustain = 0.1;
     double release = 0.6;
-    unsigned overtones = 1;
-    double overtone_factor = 1.41;
+    unsigned overtones = 4;
+    double overtone_factor = 1;
     int s = as->samples;
     int duration = audio_note_duration(as);
-    as->note -= 1 - as->note % 2;
+    as->note++;
+    duration += audio_note_duration(as);
+    if (as->note % 2 == 1) {
+        as->note--;
+        s += audio_note_duration(as);
+    }
+    as->note++;
     double hz = audio_note_hz(as);
     double u = (double)s / duration;
     double v = 0;
-    double f = 1.0 / 2.0;
+    double f = 1.0 / 4.0;
     f *= smoothstep(0, attack, u);
     f *= 1 - smoothstep(attack + sustain, attack + sustain + release, u);
     for (unsigned i = 1; i <= overtones; i++) {
@@ -189,74 +191,21 @@ double audio_gen_2(audio_state *as) {
     as->note -= as->note % 2;
     double attack = 0.05;
     double sustain = 0.0;
-    double release = 0.9;
-    unsigned overtones = 16;
-    double overtone_factor = 1.41;
+    double release = 0.5;
+    unsigned overtones = 4;
+    double overtone_factor = 1.5;
     int s = as->samples;
     double hz = audio_note_hz(as);
     int duration = audio_note_duration(as);
     double u = (double)s / duration;
     double v = 0;
-    double f = 1.0 / 4.0;
+    double f = 1.0 / 3.0;
     f *= smoothstep(0, attack, u);
     f *= 1 - smoothstep(attack + sustain, attack + sustain + release, u);
     for (unsigned i = 1; i <= overtones; i++) {
         v += audio_gen_note_sample(s, hz * i) / pow(overtone_factor, (double)i);
     }
     v *= f;
-    return v;
-}
-
-// low-frequency dramatic
-double audio_gen_3(audio_state *as) {
-    double attack = 0.01;
-    double sustain = 0.175;
-    double release = 0.820;
-    double vibrato = 0.8;
-    int vibrato_factor = 3;
-    unsigned overtones = 1;
-    double overtone_factor = 1;
-    int s = as->samples;
-    int n = as->note;
-    for (int i = 0; i < n % 2; i++) {
-        as->note = i;
-        s += audio_note_duration(as);
-    }
-    as->note = 1;
-    int duration = audio_note_duration(as);
-    as->note = 0;
-    duration += audio_note_duration(as);
-    double hz = audio_note_hz(as);
-    double u = (double)s / duration;
-    double v = 0;
-    double f = 1.0 / 2.5;
-    double fhz = smoothstep(0, attack, u);
-    fhz *= 1 - smoothstep(attack + sustain, attack + sustain + release, u);
-    f *= pow(sin(fhz * TAU * 0.25), 0.5);
-    f *= smoothstep(0, attack, u);
-    f *= 1 - smoothstep(attack + sustain, attack + sustain + release, u);
-    for (unsigned i = 1; i <= overtones; i++) {
-        v += audio_gen_note_sample(s, hz * i) / pow(overtone_factor, (double)i);
-    }
-    v *= f;
-    as->scale = 12 * vibrato_factor;
-    hz = audio_note_hz(as);
-    v *= (audio_gen_note_sample(s, hz) + 1.0) * 0.5 * vibrato + 1 - vibrato;
-    /*
-    audio_state asrw = *as;
-    if (as->note % 2 == 0) {
-    asrw.scale += 4;
-    hz = audio_note_hz(&asrw);
-    v += f * audio_gen_note_sample(as->samples, hz);
-    asrw.scale += 3;
-    hz = audio_note_hz(&asrw);
-    v += f * audio_gen_note_sample(as->samples, hz);
-    } else {
-    v *= 3;
-    }
-    */
-    //v -= 1;
-    //v = smoothstep(0, 0.5, 0.5 - abs(t - 0.5)) * 2 - 1;
     return v;
 }
 
@@ -266,8 +215,6 @@ int16_t audio_gen(const audio_state *as) {
     v += audio_gen_1(&asrw);
     asrw = *as;
     v += audio_gen_2(&asrw);
-    asrw = *as;
-    v += audio_gen_3(&asrw);
     v = clamp(v, -1, 1);
     return (int16_t)(v * 0x6000);
 }
